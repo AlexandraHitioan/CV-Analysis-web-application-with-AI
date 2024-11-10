9a4daa171e45c599cd329895daeb3b56b5e57bbf
PaulHarangusAndrei <147636835+PaulHarangusAndrei@users.noreply.github.com>
2024-11-10 21:03:10 +0200

    Upload MODAL

--- App-frontEnd/src/components/Inputs/Forms/DocumentForm/DocumentForm.tsx
+++ App-frontEnd/src/components/Inputs/Forms/DocumentForm/DocumentForm.tsx
@@ -8,7 +8,7 @@ export const DocumentForm = () =>{
     const [documentName,setDocumentName] = useState("")
     const [file,setFile] = useState("")
     return (
-        <div>
+        <div style={{display:"grid",width:"17vw"}}>
             <Input placeholder={"Document Name"} value={documentName}/>
             <Divider/>
             <UploadButton settingFunction={setFile} fileType={".pdf"}></UploadButton>
