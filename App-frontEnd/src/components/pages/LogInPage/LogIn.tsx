65657083346c7f4c8a9f24ed4d00a1baede043ce
AlexandraHitioan <alexandra.hitioan22@gmail.com>
2024-11-10 17:44:20 +0200

    Designed the Login page; needs further alterations in the Input component

--- App-frontEnd/src/components/pages/LogInPage/LogIn.tsx
+++ App-frontEnd/src/components/pages/LogInPage/LogIn.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './LogIn.css';
 import {LogInForm} from "../../Inputs/Forms/LogInForm/LogInForm"
-import {Image} from "semantic-ui-react";
+import {Button, ButtonContent, Icon, Image} from "semantic-ui-react";
 import {useNavigate} from "react-router-dom";
 
 export const LogIn = () => {
@@ -11,12 +11,42 @@ export const LogIn = () => {
         navigate("/register");
     }
 
+    // return (
+    //     <div className={"backGround"}>
+    //         <div className={"LogInContainerLogIn"}>
+    //             <Image src='https://cdn.discordapp.com/attachments/1219648191474176000/1300441864662683729/iepuras_degerat.png?ex=67222bdd&is=6720da5d&hm=8000a6770caef12b598b6175abe7a1390247fdd1acbedd071c61d8b996b46aa7&' className={"LogoLogIn"} size='small' wrapped />
+    //             <LogInForm></LogInForm>
+    //             <a onClick={navigateToRegister} className={"hyperToRegister"}>Don't have an account?go to register</a>
+    //         </div>
+    //     </div>
+    // );
     return (
-        <div className={"backGround"}>
-            <div className={"LogInContainerLogIn"}>
-                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' className={"LogoLogIn"} size='small' wrapped />
-                <LogInForm></LogInForm>
-                <a onClick={navigateToRegister} className={"hyperToRegister"}>Don't have an account?go to register</a>
+        <div className="backGround">
+            <div>
+                <div className="leftSection">
+                    <h1 className="h1">Hello there!</h1>
+                    <p className="bodyText">Log into your account if you already have one. If not, click the button below to register</p>
+                    {/*<button onClick={navigateToRegister} className="hyperToRegister">*/}
+                    {/*    Register*/}
+                    {/*</button>*/}
+                    <Button animated type="submit" className={"RegisterButton"} onClick={navigateToRegister}>
+                        <ButtonContent visible>Register</ButtonContent>
+                        <ButtonContent hidden>
+                            <Icon name='arrow right' />
+                        </ButtonContent>
+                    </Button>
+                </div>
+                <div className="rightSection">
+                    <div className="LogInContainerLogIn">
+                        <Image
+                            src="https://cdn.discordapp.com/attachments/1219648191474176000/1300441864662683729/iepuras_degerat.png?ex=67222bdd&is=6720da5d&hm=8000a6770caef12b598b6175abe7a1390247fdd1acbedd071c61d8b996b46aa7&"
+                            className="LogoLogIn"
+                            size="small"
+                            wrapped
+                        />
+                        <LogInForm />
+                    </div>
+                </div>
             </div>
         </div>
     );
