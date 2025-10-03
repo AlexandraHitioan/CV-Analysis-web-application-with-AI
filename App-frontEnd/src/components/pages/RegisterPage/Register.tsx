c0f57bde82bc5e375c6c8cd318d938588ffeac3b
AlexandraHitioan <alexandra.hitioan22@gmail.com>
2025-10-03 16:11:33 +0300

    Add project to github

--- App-frontEnd/src/components/pages/RegisterPage/Register.tsx
+++ App-frontEnd/src/components/pages/RegisterPage/Register.tsx
@@ -7,7 +7,13 @@ export const Register = () => {
     return (
         <div className={"backGround"}>
             <div className={"RegisterContainer"}>
-                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' className={"LogoReg"} size='small' wrapped />
+                <Image
+                    // src="https://cdn.discordapp.com/attachments/1219648191474176000/1300441864662683729/iepuras_degerat.png?ex=67222bdd&is=6720da5d&hm=8000a6770caef12b598b6175abe7a1390247fdd1acbedd071c61d8b996b46aa7&"
+                    src = "App-frontEnd/public/Logo Proiect.png"
+                    className="LogoRegister"
+                    size="small"
+                    wrapped
+                />
                 <RegisterForm></RegisterForm>
             </div>
         </div>
